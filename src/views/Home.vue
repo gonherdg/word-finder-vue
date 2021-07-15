4fe3161b5e5222e9dc6b84f84212a976c331145e
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2021-07-15 20:25:44 -0300

    feat: responsive home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,79 @@
 <template>
-  <div class="home">
-    <div class="content" :class="{ 'content-no-data': this.justLoaded }">
-      <h1>Fast Word Finder</h1>
-      <strong class="instruccions"
-        >Find words entering some of it's letters:</strong
-      >
-      <div>
-        <div>
-          <label>starts with...</label>
+  <div class="home flex flex-col">
+    <div class="content h-full" :class="{ 'min-h-screen': this.justLoaded }">
+      <h1 class="text-5xl my-10 p-1">Fast Word Finder</h1>
+      <div class="m-2">
+        <strong class="text-lg"
+          >Find words entering some of it's letters:</strong
+        >
+      </div>
+      <div class="flex flex-col sm:flex-row justify-around">
+        <div class="flex flex-col mx-auto sm:ml-auto sm:mr-4 md:mr-8">
+          <label class="mt-4">starts with...</label>
           <input
             v-on:change="findWords()"
             v-model="starts"
-            placeholder="starts with..."
-            class="field"
+            placeholder=""
+            class="rounded-full w-48 text-md text-center p-2 m-1 uppercase"
           />
-          <label>ends with...</label>
-          <input v-model="ends" placeholder="ends with..." class="field" />
+          <label class="mt-4">ends with...</label>
+          <input
+            v-on:change="findWords()"
+            v-model="ends"
+            placeholder=""
+            class="rounded-full w-48 text-md text-center p-2 m-1 uppercase"
+          />
         </div>
-        <div>
-          <label>contains...</label>
+        <div class="flex flex-col mx-auto sm:mr-auto sm:ml-4 md:ml-8">
+          <label class="mt-4">contains...</label>
           <input
             v-on:change="findWords()"
             v-model="contains"
-            placeholder="contains..."
-            class="field"
+            placeholder=""
+            class="rounded-full w-48 text-md text-center p-2 m-1 uppercase"
           />
-          <label>it's length...</label>
+          <label class="mt-4">it's length...</label>
           <input
             v-on:change="findWords()"
             type="number"
             v-model.number="length"
-            placeholder="lenght..."
-            class="field"
+            placeholder=""
+            class="rounded-full w-48 text-md text-center p-2 m-1 uppercase"
           />
         </div>
-        <button v-on:click="findWords()" class="search-btn">SEARCH</button>
       </div>
+      <button
+        v-on:click="findWords()"
+        class="
+          w-9/12
+          sm:w-7/12
+          md:w-5/12
+          lg:w-80
+          mx-auto
+          bg-white
+          p-2
+          m-10
+          rounded-full
+        "
+      >
+        SEARCH
+      </button>
     </div>
 
     <loading
+      class="mx-auto"
       v-bind:active="isLoading"
       :can-cancel="true"
       :on-cancel="onCancel"
       :is-full-page="true"
     />
-    <div class="words-container" v-if="words !== {} && !justLoaded">
+    <div class="mb-10 p-0" v-if="words !== {} && !justLoaded">
       <ul>
-        <li class="word" v-for="(w, key) in words" v-bind:key="key">
+        <li
+          class="m-1 py-1 px-2 bg-gray-800 rounded-lg text-white inline-block"
+          v-for="(w, key) in words"
+          v-bind:key="key"
+        >
           <a
             target="_blank"
             style="color: white"
@@ -57,10 +84,9 @@
         </li>
       </ul>
     </div>
-    <p style="color: #222; margin: 2rem" v-if="!words.length && !justLoaded">
+    <p class="m-10 text-gray-800" v-if="!words.length && !justLoaded">
       No words were found.
     </p>
-    <div class="some-space"></div>
 
     <CommonFooter />
   </div>
@@ -143,104 +169,11 @@ export default {
 @media only screen and (min-width: 10px) {
   .home {
     background-color: #42b983;
-    color: #ffffff;
     font-family: "Source Sans Pro", sans-serif;
-  }
-
-  .content-no-data {
-    min-height: 100vh;
   }
 
   .content {
-    height: 100%;
     font-family: "Source Sans Pro", sans-serif;
-  }
-
-  h1 {
-    font-size: 3rem;
-    margin-top: 0px;
-    padding: 1.5rem;
-  }
-
-  .instruccions {
-    font-size: 1.2rem;
-    margin: 2rem;
-  }
-
-  .finder-input {
-    font-size: 1.5rem;
-    text-align: center;
-    text-transform: uppercase;
-    padding: 1rem;
-    min-width: 25rem;
-    border-radius: 25px;
-    border-style: none;
-    margin: 2rem;
-  }
-
-  .field {
-    font-size: 1rem;
-    text-align: center;
-    padding: 0.5rem;
-    width: 15rem;
-    border-style: none;
-    border-radius: 25rem;
-    margin: 1rem;
-  }
-
-  .search-btn {
-    font-size: 1rem;
-    text-align: center;
-    padding: 0.5rem;
-    width: 34rem;
-    border-style: none;
-    border-radius: 25px;
-    margin: 1rem;
-    background-color: white;
-  }
-
-  .inline {
-    display: inline;
-  }
-
-  .block {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-  .words-container {
-    margin: 1.5rem;
-    padding: 1.5rem;
-  }
-
-  .word {
-    padding: 0.4rem;
-    background-color: #222;
-    border-radius: 10px;
-    color: white;
-    margin: 0.2rem;
-  }
-
-  .some-space {
-    height: 2rem;
-    background-color: #42b983;
-  }
-
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: black;
-    text-decoration: none;
   }
 }
 </style>
