cc0c02195b671c8a9547a3e671d1cd17ace73747
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2021-07-15 21:45:44 -0300

    feat: responsive header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="header">
-    <div class="left-side">
+  <div class="header flex flex-row justify-between">
+    <div class="flex-1">
       <img
-        class="logo-img"
+        class="mx-auto object-cover object-center w-64 h-24"
         alt="Fast Word Finder"
         src="../assets/logo_black.png"
       />
     </div>
 
-    <div class="right-side">
-      <router-link class="option" to="/">Home</router-link> |
-      <router-link class="option" to="/about">About</router-link>
+    <div class="flex-1 my-auto">
+      <router-link class="px-3 py-1" to="/">Home</router-link> |
+      <router-link class="px-3 py-1" to="/about">About</router-link>
     </div>
   </div>
 </template>
@@ -30,55 +30,14 @@ export default {
 <style scoped>
 .header {
   background-color: #42b983;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
   font-family: "Source Sans Pro", sans-serif;
 }
 
-.left-side {
-  width: 16rem;
-  position: left;
-  margin-left: 10%;
-  text-align: left;
-}
-
 .right-side {
-  text-align: right;
-  position: right;
-  margin-right: 10%;
-  margin-top: auto;
-  margin-bottom: auto;
+  text-align: center;
+  position: center;
 }
 
-.option {
-  padding-left: 1.2rem;
-  padding-right: 1.2rem;
-  padding-top: 0.6rem;
-  padding-bottom: 0.6rem;
-  border-radius: 5px;
-  border: none;
-  background-color: transparent;
-  color: black;
-  font-size: medium;
-  font-weight: 600;
-}
-
-.logo-img {
-  object-fit: cover;
-  object-position: center;
-  width: 16rem;
-  height: 6rem;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
